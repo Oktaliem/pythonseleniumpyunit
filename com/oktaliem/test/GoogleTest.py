d24594a34ca68110b941ce1bb084ac6516b8f19d
Okta Liem <surya_diharja@yahoo.co.id>
2020-08-15 19:22:37 +0800

    add set up and tear down
    how to run the test:
    
    go to test folder and run this syntax
    
    $ python GoogleTest.py -v

--- com/oktaliem/test/GoogleTest.py
+++ com/oktaliem/test/GoogleTest.py
@@ -6,21 +6,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class GoogleTest(unittest.TestCase):
 
-    @staticmethod
-    def test_reopen_google_url_and_close():
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get("https://www.google.com")
-        driver.implicitly_wait(30000)
-        print(driver.current_url)
-        driver.quit()
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
 
-    @staticmethod
-    def test_open_google_url_and_close():
-        driver = webdriver.Chrome(ChromeDriverManager().install())
-        driver.get("https://www.google.com")
-        driver.implicitly_wait(30000)
-        print(driver.current_url)
-        driver.quit()
+    def test_reopen_google_url_and_close(self):
+        self.driver.get("https://www.google.com")
+        self.driver.implicitly_wait(30000)
+        print(self.driver.current_url)
+
+    def test_open_google_url_and_close(self):
+        self.driver.get("https://www.google.com")
+        self.driver.implicitly_wait(30000)
+        print(self.driver.current_url)
+
+    def tearDown(self):
+        self.driver.quit()
 
 
 if __name__ == "__main__":
