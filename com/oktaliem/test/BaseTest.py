9ab503b9d8b20d098c4bbcd549518fd8877cb7c3
Okta Liem <surya_diharja@yahoo.co.id>
2020-08-15 21:29:45 +0800

    add base test
    - add base test
    
    run via command line masih error
    $ python com/oktaliem/test/GoogleTest.py -v

---
+++ com/oktaliem/test/BaseTest.py
@@ -0,0 +1,16 @@
+import unittest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class BaseTest(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
--- com/oktaliem/test/GoogleTest.py
+++ com/oktaliem/test/GoogleTest.py
@@ -1,13 +1,9 @@
+from com.oktaliem.test import BaseTest
+
 import unittest
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
-
-class GoogleTest(unittest.TestCase):
-
-    def setUp(self):
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+class GoogleTest(BaseTest.BaseTest):
 
     def test_reopen_google_url_and_close(self):
         self.driver.get("https://www.google.com")
@@ -18,9 +14,6 @@ class GoogleTest(unittest.TestCase):
         self.driver.get("https://www.google.com")
         self.driver.implicitly_wait(30000)
         print(self.driver.current_url)
-
-    def tearDown(self):
-        self.driver.quit()
 
 
 if __name__ == "__main__":
